9e4c83098902393c15a4fcb83622c4a8c316ec94
kinguva7229 <kinguva7229@gmail.com>
2025-11-09 22:47:20 -0500

    changed heirarchy

--- favorite-media/api/movies.js
+++ favorite-media/api/movies.js
@@ -6,11 +6,7 @@ export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
 
   try {
-    const filePath = path.join(process.cwd(), "movies.json");
-    console.log("📁 process.cwd():", process.cwd());
-    console.log("📄 Looking for:", filePath);
-    console.log("📦 Exists?", fs.existsSync(filePath));
-
+    const filePath = path.join(process.cwd(), "api", "movies.json"); 
     const data = fs.readFileSync(filePath, "utf-8");
     const parsed = JSON.parse(data);
     res.status(200).json(parsed.movies);
@@ -19,6 +15,7 @@ export default async function handler(req, res) {
     res.status(500).json({ error: "Failed to load movie data" });
   }
 }
+
 
 
 
